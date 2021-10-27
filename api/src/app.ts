580f5c4296ce0ad9ac55f3891777d952b0d350e8
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-10-27 14:15:22 +0200

    Enable caching for App assets
    All assets transformed by Vite are fingerprinted, so they can be cached indefinitely.
    The html entrypoint should never be cached to always import the latest assets.
    The only downside to this is that files from the "public/" folder are also cached for at least a year.

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,6 +3,7 @@ import express, { RequestHandler } from 'express';
 import fse from 'fs-extra';
 import path from 'path';
 import qs from 'qs';
+import { ServerResponse } from 'http';
 
 import activityRouter from './controllers/activity';
 import assetsRouter from './controllers/assets';
@@ -135,11 +136,19 @@ export default async function createApp(): Promise<express.Application> {
 		const html = await fse.readFile(adminPath, 'utf8');
 		const htmlWithBase = html.replace(/<base \/>/, `<base href="${adminUrl.toString({ rootRelative: true })}/" />`);
 
-		app.get('/admin', (req, res) => res.send(htmlWithBase));
-		app.use('/admin', express.static(path.join(adminPath, '..')));
-		app.use('/admin/*', (req, res) => {
+		const sendHtml: RequestHandler = (_req, res) => {
+			res.setHeader('Cache-Control', 'no-cache');
+			res.setHeader('Vary', 'Origin, Cache-Control');
 			res.send(htmlWithBase);
-		});
+		};
+		const setStaticHeaders = (res: ServerResponse) => {
+			res.setHeader('Cache-Control', 'max-age=31536000, immutable');
+			res.setHeader('Vary', 'Origin, Cache-Control');
+		};
+
+		app.get('/admin', sendHtml);
+		app.use('/admin', express.static(path.join(adminPath, '..'), { setHeaders: setStaticHeaders }));
+		app.use('/admin/*', sendHtml);
 	}
 
 	// use the rate limiter - all routes for now
